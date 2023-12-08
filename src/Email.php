bd8dd1e7fddf5a6fa2a1f688b7eec68543d9d25e
Victor Dracxler <victordracxler@poli.ufrj.br>
2023-12-08 16:34:27 -0300

    feat: move index to /test

--- src/Email.php
+++ src/Email.php
@@ -10,25 +10,25 @@ class Email
 {
     private $mail = \stdClass::class;
 
-    public function __construct()
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
     {
         $this->mail = new PHPMailer(true);
 
         //Server settings
-        $this->mail->SMTPDebug = 2;                      //Enable verbose debug output
-        $this->mail->isSMTP();                                            //Send using SMTP
-        $this->mail->Host = 'smtp.office365.com';                     //Set the SMTP server to send through
-        $this->mail->SMTPAuth = true;                                   //Enable SMTP authentication
-        $this->mail->Username = 'victor.dracxler@rwesistemas.com.br';                     //SMTP username
-        $this->mail->Password = 'teste@123';                               //SMTP password
-        $this->mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-        $this->mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $this->mail->SMTPDebug = $smtpDebug;                      //Enable verbose debug output
+        $this->mail->isSMTP();                                    //Send using SMTP
+        $this->mail->Host = $host;                                //Set the SMTP server to send through
+        $this->mail->SMTPAuth = true;                             //Enable SMTP authentication
+        $this->mail->Username = $user;                            //SMTP username
+        $this->mail->Password = $pass;                            //SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;                    //Enable implicit TLS encryption
+        $this->mail->Port = $port;                                //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         $this->mail->CharSet = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
 
-        $this->mail->setFrom('victor@gustavoweb.me', 'Equipe GustavoWeb');
+        $this->mail->setFrom($setFromEmail, $setFromName);
 
     }
 
--- test/index.php
+++ test/index.php
@@ -1,11 +1,10 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
-
+require __DIR__ . '/../vendor/autoload.php';
 use Notification\Email;
 
 
-$novoEmail = new Email;
+$novoEmail = new Email(2, "mail.gustavoweb.me", "sender@gustavoweb.com", "12345678", 'tls', '587', 'gustavo@gustavoweb.me', 'equipe gustavoweb');
 $novoEmail->sendMail("Assunto de teste", "<p>Este é um email de <b>teste</b></p>", "victor@gustavoweb.me", "victor remetente", "victordracxler@hotmail.com", "victor destinatario");
 
 var_dump($novoEmail);
